1c1a12dc8a4b03825901d52591a16daaf6fd7928
evanrolfe <evanrolfe@gmail.com>
2012-10-21 15:39:10 +0100

    dirty version

--- fuel/app/classes/controller/data.php
+++ fuel/app/classes/controller/data.php
@@ -16,20 +16,99 @@ class Controller_Data extends MyController
 		$this->template->content = View::forge('data/index', $data);		
 	}
 
+	public function action_cols()
+	{
+		$columns = DB::list_columns('asnames');		
+
+		echo "<pre>";
+		print_r($columns);
+		echo "</pre>";
+
+		exit;
+	}
+
 	public function action_export()
 	{
 		$date = Date::forge()->format("%d-%m-%Y %H:%m");
 		header('Content-type: text/plain');
-		header('Content-Disposition: attachment; filename="backup at '.$date.'.sql"');	
+		//header('Content-Disposition: attachment; filename="backup at '.$date.'.sql"');	
 
 		$backup = Model_Backup::forge(array(
 			'note' =>'',
 			'type' => 'export',
 		));
 
-		$backup->save();
+		//$backup->save();
 
-		echo passthru('mysqldump --host=localhost --user=root --password=pass fuel_dev');
+		//echo passthru('mysqldump --host=localhost --user=root --password=pass fuel_dev');
+
+		//1. Get the table names
+		$tables_res = DB::query("SHOW TABLES", DB::SELECT)->execute();
+		$tables = array();
+		foreach($tables_res as $table_arr)
+		{
+			$tables[] = $table_arr['Tables_in_yachtfractions'];		
+		}
+
+
+
+		foreach($tables as $table)
+		{
+			if(in_array($table, array('yachtshares','buyers','formfields_buyer','emailtemplates','shares','sessions', 'migration')))
+				continue;
+
+			echo "INSERT INTO `".$table."` (";
+			$columns = DB::list_columns($table);
+
+			$i=1;
+			foreach($columns as $key => $col)
+			{
+				echo "`".$key."`";
+				if($i < count($columns))
+					echo ", ";
+				$i++;
+			}
+			
+			echo ") VALUES ";
+	
+			$rows = DB::query('SELECT * FROM `'.$table.'`',DB::SELECT)->execute();
+
+			$j=1;
+			foreach($rows as $row)
+			{
+				echo "(";
+				$i=1;
+				foreach($columns as $key => $col)
+				{
+					$val = (is_null($row[$key])) ? "NULL" : $row[$key];
+
+
+					if($col['type'] == 'string')
+					{
+						//$val = Security::clean($val, array('strip_tags', 'htmlentities'));
+						echo "'".$val."'";
+					}else{
+						echo $val;
+					}
+
+					if($i < count($columns))
+						echo ", ";
+					$i++;
+				}
+
+				echo ")";
+
+				if($j < count($rows))
+				{
+					echo ",";
+				}else{
+					echo ";";
+				}
+				$j++;
+			}
+		}
+		exit;
+
 		//Response::redirect('data');
 	}
 

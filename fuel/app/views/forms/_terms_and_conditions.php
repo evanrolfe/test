3707a6f80a18ef646ecb71095ebd77292c4fe682
evanrolfe <evanrolfe@gmail.com>
2012-10-25 18:24:02 +0100

    added terms and conditions form field

---
+++ fuel/app/views/forms/_terms_and_conditions.php
@@ -0,0 +1,7 @@
+	<div class="formRow">
+	    <div class="grid3"><label><?= $field->label; ?></label></div>
+	    <div class="grid9 noSearch" align="left">
+			<input type="checkbox" class="required" />
+		</div>
+		<div class="clear"></div>
+	</div>

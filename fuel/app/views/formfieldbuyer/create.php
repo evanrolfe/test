a40d91806d8991c83258e395a9c735d7e6762edd
evanrolfe <evanrolfe@gmail.com>
2012-10-25 21:37:11 +0100

    added more options for field type in formfieldbuyer/create

--- fuel/app/views/formfieldbuyer/create.php
+++ fuel/app/views/formfieldbuyer/create.php
@@ -92,6 +92,8 @@ function add_option()
 					<option value="textarea">Mutli line text field</option>
 					<option value="text_fraction">Single line text field for fractions (i.e. share size)</option>
 					<option value="dropdown">Dropdown box</option>
+					<option value="length">Measure of length</option>
+					<option value="terms_and_conditions">Terms and conditions checkbox</option>
 			</select>
 		</div>
         <div class="clear"></div>
--- fuel/app/views/forms/_terms_and_conditions.php
+++ fuel/app/views/forms/_terms_and_conditions.php
@@ -2,6 +2,9 @@
 	    <div class="grid3"><label><?= $field->label; ?></label></div>
 	    <div class="grid9" align="left">
 			<input type="checkbox" name="terms" id="terms" class="required">
+	<? if(!empty($field->description)): ?>
+		<span><?=$field->description;?></span>
+	<? endif;?>
 		</div>
 		<div class="clear"></div>
 	</div>

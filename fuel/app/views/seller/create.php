ceabf6940056a623b08b4ae11eac04c41552455a
evanrolfe <evanrolfe@gmail.com>
2012-10-28 17:35:56 +0000

    added an installer controller which simply creates a new admin user according to form input

--- fuel/app/views/seller/create.php
+++ fuel/app/views/seller/create.php
@@ -53,7 +53,7 @@ $(document).ready(function(){
 	<div class="whead">
 		<h6 style="opacity: 0.0;">-</h6>
 		<div style='text-align: right;'>
-			<button class="buttonS bGreen" style="margin: 6px 6px;">Register</button>
+			<button class="buttonS bGreen" style="margin: 6px 6px;" type="submit">Register</button>
 		</div>
 		<div class="clear"></div>
 	</div>

2dcb7f6b73d2da0dc3e7ca2b040f46fa78a2bc66
evanrolfe <evanrolfe@gmail.com>
2012-11-15 13:41:36 +0000

    text_fraction can now be set to required/not from admin/formfieldbuyer/edit

--- fuel/app/views/forms/_text_fraction.php
+++ fuel/app/views/forms/_text_fraction.php
@@ -1,7 +1,7 @@
 <div class="formRow">
     <div class="grid3"><label><?= $field->label; ?></label></div>
     <div class="grid9 noSearch" align="left">
-		<input type='text' class="required number" name="<?=$field->tag;?>_num" style='width: 45px;' value="<?=$value['num'];?>" /> / <input class="required number" type='text' name="<?=$field->tag;?>_den" style='width: 45px;' value="<?=$value['den']?>" />
+		<input type='text' class="<?=$field->validation;?> number" name="<?=$field->tag;?>_num" style='width: 45px;' value="<?=$value['num'];?>" /> / <input class="required number" type='text' name="<?=$field->tag;?>_den" style='width: 45px;' value="<?=$value['den']?>" />
 	<? if(!empty($field->description)): ?>
 		<span><?=$field->description;?></span>
 	<? endif;?>

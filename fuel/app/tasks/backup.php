04082a4914ba3d6b050b2ec741e8ef261e0e28a7
Evan Rolfe <evanrolfe@gmail.com>
2013-01-02 19:26:56 +0000

    first version of rake task to send backup to email.

--- fuel/app/tasks/backup.php
+++ fuel/app/tasks/backup.php
@@ -4,7 +4,7 @@ namespace Fuel\Tasks;
 class Backup
 {
 
-    public function run($email = "yachtfractions@gmail.com")
+    public function run($to_email = "evanrolfe@gmail.com")
     {
 			$date = \Date::forge()->format("%d-%m-%Y %H:%m");
 
@@ -13,16 +13,18 @@ class Backup
 
 			//2. Populate email data
 			$email->from('yachtfractions@evanrolfe.info', 'Yacht Fractions');
-			$email->to(array( $email));
+			$email->to($to_email);
 			$email->subject("[yachtfractions] Site Backup File Attached");	
 			$email->body("The site backup file is attached.");
 
-			$email->string_attach('asfd', "backup at ".$date.".sql");
+			$backup = $this->get_sql_string();
+
+			$email->string_attach($backup, "backup at ".$date.".sql");
 
 			try
 			{
 				$email->send();
-				$return = "Backup successfully sent.";
+				$return = "Backup successfully sent to: ".$to_email;
 			}
 			catch(\EmailValidationFailedException $e)
 			{
@@ -35,4 +37,72 @@ class Backup
 
 			echo $return;
     }
+
+	public function get_sql_string()
+	{
+		//1. Get the table names
+		$tables = array('actionsteps','asnames','buyers','emailtemplates','formfields_buyer','images','yachtshares');
+
+		$out = "";
+		foreach($tables as $table)
+		{
+			if(in_array($table, array('shares','sessions', 'migration','backups','users'))) //'yachtshares','buyers','formfields_buyer','emailtemplates',
+				continue;
+
+			$out .= "TRUNCATE `".$table."`;";
+
+			$out .= "INSERT INTO `".$table."` (";
+			$columns = \DB::list_columns($table);
+
+			$i=1;
+			foreach($columns as $key => $col)
+			{
+				$out .= "`".$key."`";
+				if($i < count($columns))
+					$out .= ", ";
+				$i++;
+			}
+			
+			$out .= ") VALUES ";
+	
+			$rows = \DB::query('SELECT * FROM `'.$table.'`',\DB::SELECT)->execute();
+
+			$j=1;
+			foreach($rows as $row)
+			{
+				$out .= "(";
+				$i=1;
+				foreach($columns as $key => $col)
+				{
+					$val = (is_null($row[$key])) ? "NULL" : $row[$key];
+
+
+					if($col['type'] == 'string')
+					{
+						//$val = Security::clean($val, array('strip_tags', 'htmlentities'));
+						$val = addslashes($val);
+						$out .= "'".$val."'";
+					}else{
+						$out .= $val;
+					}
+
+					if($i < count($columns))
+						$out .= ", ";
+					$i++;
+				}
+
+				$out .= ")";
+
+				if($j < count($rows))
+				{
+					$out .= ",";
+				}else{
+					$out .= ";";
+				}
+				$j++;
+			}
+		}
+
+		return $out;		
+	}    
 }

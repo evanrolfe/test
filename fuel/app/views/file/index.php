ad67e3724095b3bb08b50b1d5cf802fedc86605c
Chris Rolfe <c.rolfe@ais.at>
2013-04-16 09:48:44 +0200

    Photo upload added thumbnails and formatting

--- fuel/app/views/file/index.php
+++ fuel/app/views/file/index.php
@@ -3,10 +3,48 @@
 <? else: ?>
 	<?= render('buyer/_nav', array('buyer' => $item)); ?>
 <? endif; ?>
+<form action="<?= Uri::create('file/upload'); ?>" method="POST" enctype="multipart/form-data">
+<input type="hidden" name="belongs_to_id" value="<?=$item->id;?>" />
+<input type="hidden" name="belongs_to" value="<?=$type;?>" />
+<div class="widget fluid">
+    <div class="whead">
+		<h6>Upload File</h6>
+		<div class="clear"></div>
+	</div>
+
+	<div class="formRow">
+	    <div class="grid3"><label>Select a File:</label></div>
+	    <div class="grid9" align="left">
+			<input type='file' name="file"/>
+		</div>
+		 <div class="clear"></div>
+	</div>
+
+	<div class="formRow">
+	    <div class="grid3"><label>File Type:</label></div>
+	    <div class="grid9 noSearch" align="left">
+			<select class='select' name="type">
+					<option value="public_gallery">Public gallery photo for website</option>
+					<option value="public_header">Public header photo for website</option>
+					<option value="private">Private document or photo</option>
+			</select>
+		</div>
+		 <div class="clear"></div>
+	</div>
+
+	<div class="whead">
+		<h6 style="opacity: 0.0;">-</h6>
+		<div style='text-align: right;'>
+			<button class="buttonS bGreen" style="margin: 6px 6px;" type="submit">Upload</button>
+		</div>
+		<div class="clear"></div>
+	</div>
+</div>
+</form>
 
 <div class="widget fluid">
     <div class="whead">
-		<h6>Files</h6>
+		<h6>Files Already Uploaded</h6>
 		<div class="clear"></div>
 	</div>
 
@@ -17,7 +55,7 @@
 <script type="text/javascript">
 $(function(){
 	var $dialog = $('<div></div>')
-		.html('Are you sure you want to this file?')
+		.html('Are you sure you want to delete this file?')
 		.dialog({
 			autoOpen: false,
 			title: "Deleting File:",
@@ -40,7 +78,15 @@ $(function(){
 });
 </script>
 
-				<a href="#" id="file_<?=$file->id;?>">[X]</a>  <a href="<?=Uri::create('public/uploads/'.$file->url);?>" target="_blank"><?= $file->url; ?></a><br>
+
+				
+                <table width="400" border="1" align="left">
+                  <tr align="left" valign="top">
+                    <td><img src="<?=Uri::create('public/uploads/'.$file->url);?>" width="100" height="100" /></td>
+                    <td><a href="#" id="file_<?=$file->id;?>">[X]</a>  <a href="<?=Uri::create('public/uploads/'.$file->url);?>" target="_blank"><?= $file->url; ?></a></td>
+                    </tr>
+                </table>
+                
 			<? endforeach; ?>
 		<? else: ?>
 			There are no files uploaded for this <?=$type; ?>.
@@ -50,41 +96,3 @@ $(function(){
 </div>
 </form>
 
-<form action="<?= Uri::create('file/upload'); ?>" method="POST" enctype="multipart/form-data">
-<input type="hidden" name="belongs_to_id" value="<?=$item->id;?>" />
-<input type="hidden" name="belongs_to" value="<?=$type;?>" />
-<div class="widget fluid">
-    <div class="whead">
-		<h6>Upload File</h6>
-		<div class="clear"></div>
-	</div>
-
-	<div class="formRow">
-	    <div class="grid3"><label>Select a File:</label></div>
-	    <div class="grid9" align="left">
-			<input type='file' name="file"/>
-		</div>
-		 <div class="clear"></div>
-	</div>
-
-	<div class="formRow">
-	    <div class="grid3"><label>File Type:</label></div>
-	    <div class="grid9 noSearch" align="left">
-			<select class='select' name="type">
-					<option value="private">Private document or photo</option>
-					<option value="public_header">Public header photo for website</option>
-					<option value="public_gallery">Public gallery photo for website</option>
-			</select>
-		</div>
-		 <div class="clear"></div>
-	</div>
-
-	<div class="whead">
-		<h6 style="opacity: 0.0;">-</h6>
-		<div style='text-align: right;'>
-			<button class="buttonS bGreen" style="margin: 6px 6px;" type="submit">Upload</button>
-		</div>
-		<div class="clear"></div>
-	</div>
-</div>
-</form>
